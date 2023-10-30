87b56c971ffca14d6412065e0e375f8a0472f7b7
Dylan Alvandi <g.dylanalvandi@gmail.com>
2023-10-30 19:51:19 +0100

    create who page

--- dylanalvandi-site/src/app/who/page.tsx
+++ dylanalvandi-site/src/app/who/page.tsx
@@ -1,9 +1,8 @@
-import React from 'react'
+import React from "react";
+import { NAVBAR_HEIGHT } from "../sizes";
 
-type Props = {}
+type Props = {};
 
 export default function page({}: Props) {
-  return (
-    <div>Who?</div>
-  )
+  return <div className="flex flex-col justify-center" style={{marginTop: NAVBAR_HEIGHT}}>Who?</div>;
 }
